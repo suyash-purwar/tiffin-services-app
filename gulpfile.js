6b1c47b914319ce53a8b306eb2372e3b47d062f6
suyash-purwar <purwarsuyash06@gmail.com>
2019-10-04 12:21:27 +0530

    Write gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -15,3 +15,72 @@ const minifycss = require("gulp-clean-css")
 const info = () => {
     return console.log("Gulp is running!")
 }
+
+// Send favicons to dist
+const sendFavicons = () => gulp.src("src/images/favicons/*")
+    .pipe(gulp.dest("dist/images/favicons"))
+    .pipe(browserSync.stream());
+
+// Copy all HTML files
+const copyHTML = () => {
+    return gulp.src("src/index.html")
+        .pipe(gulp.dest("dist"))
+        .pipe(browserSync.stream());
+}
+
+// Manage Styles
+const manageStyles = () => {
+    return gulp.src("src/sass/main.scss")
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
+
+    // FOR FUTURE USE
+    // const css_paths = [
+    //     "node_modules/splitting/dist/splitting.css"
+    // ]
+
+    // const sass_stream = gulp.src("src/sass/main.sass")
+    //     .pipe(sass({
+    //         outputStyle: 'compressed'
+    //     }).on('error', sass.logError))
+    //     .pipe(autoprefixer({
+    //         browsers: ['last 5 versions'],
+    //         cascade: false
+    //     }))
+
+    // const css_stream = gulp.src(css_paths)
+    //     .pipe(minifycss())
+
+    // return merge(sass_stream, css_stream)
+    //     .pipe(concat("main.css"))
+    //     .pipe(gulp.dest("dist/css"))
+    //     .pipe(browserSync.stream());
+}
+
+const bundleIndexJS = () => {
+    return gulp.src("src/js/index.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("dist/js"))
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+    gulp.watch('src/sass/**/*.scss', manageStyles);
+    gulp.watch('src/images/favicons/*', sendFavicons);
+    gulp.watch('src/*.html', copyHTML);
+    gulp.watch('src/js/*.js', bundleIndexJS);
+}
+
+exports.watch = watch
